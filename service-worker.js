8fe338d0644c26197db13bbd3c3b7f77e261eb2d
sarayourfriend <24264157+sarayourfriend@users.noreply.github.com>
2021-07-19 15:51:10 -0700

    Cache all version of city pages

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,11 @@
 ---
 ---
+const cityNames = [
+    {%- for city in site.cities -%}
+        "{{ city.slug }}",
+    {%- endfor -%}
+];
+
 self.addEventListener("install", (event) => {
     event.waitUntil(
         caches.open('v{{ site.version }}').then((cache) => {
@@ -8,12 +14,14 @@ self.addEventListener("install", (event) => {
                 "index.html",
                 "service-worker.js",
                 "not911.webmanifest",
-                "seattle.html",
                 "404.html",
                 "style.css",
                 "phone.svg",
                 "apple-touch-icon.png",
                 "favicon.ico",
+                // include all cities in both their `.html` and non `.html` forms
+                ...cityNames,
+                ...cityNames.map(name => `${name}.html`)
             ]);
         })
     );
@@ -28,7 +36,6 @@ self.addEventListener('activate', (event) => {
       caches.keys().then((cacheNames) => {
         return Promise.all(
             cacheNames.filter((cacheName) => {
-                console.log(cacheName);
                 return cacheName !== 'v{{ site.version }}';
             }).map((cacheName) => {
                 return caches.delete(cacheName);
