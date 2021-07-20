bde6af2dbe38d01b079fc74aa894f4df3f7ce979
Taylor Case <tmcase16@gmail.com>
2021-07-20 12:13:32 -0700

    add accessibility support

--- main.js
+++ main.js
@@ -14,17 +14,21 @@
         themeToggleButton.setAttribute('aria-pressed', false);
     }
 
+    // regular click
     themeToggleButton.addEventListener('click', handleThemeToggle);
 
-    function handleThemeToggle(e) {
-        rootElement.classList.toggle('light');
-        rootElement.classList.toggle('dark');
-
+    // specifically for accessibility, check if correct key has been pressed and call function
+    themeToggleButton.addEventListener('onkeydown', function(e) {
         if (e.key === " " || e.key === "Enter" || e.key === "Spacebar") { // "Spacebar" for IE11 support
             // Prevent the default action to stop scrolling when space is pressed
             e.preventDefault();
-            toggleButton(e.target);
-          }
+            handleThemeToggle();
+        }
+    });
+
+    function handleThemeToggle() {
+        rootElement.classList.toggle('light');
+        rootElement.classList.toggle('dark');
 
         const theme = rootElement.classList.contains('light') ? 'light' : 'dark';
         const ariaPressed = rootElement.classList.contains('light') ? false : true;
