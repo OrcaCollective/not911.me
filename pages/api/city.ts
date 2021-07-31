24fe10d39dc28be687a453c0ab1bff018bb5757f
sarayourfriend <24264157+sarayourfriend@users.noreply.github.com>
2021-07-31 09:44:45 -0700

    Add city api

---
+++ pages/api/city.ts
@@ -0,0 +1,34 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+import type { CityDescription, CategoryDescription, ResourceDescription } from '../../types';
+
+import { cities } from '../../cities';
+import { getResourcesForCityByCategory } from '../../utils/get-resources-for-city';
+
+interface Response {
+    city: CityDescription;
+    categories: CategoryDescription[];
+    resources: Record<string, ResourceDescription[]>;
+}
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Response | null>
+) {
+    const citySlug = req.query.slug;
+    if (!citySlug) {
+        return res.status(400).send(null);
+    }
+    const city = cities.find(c => c.slug === citySlug);
+    if (!city) {
+        return res.status(404).send(null);
+    }
+
+    const [categories, resources] = getResourcesForCityByCategory(city.slug);
+
+    return res.status(200).send({
+        city,
+        categories,
+        resources,
+    })
+}
